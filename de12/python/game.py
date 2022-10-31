7ac7c7073c039abfca61cd854578c060c75a455b
KurumiOiKang <r202202074om@jindai.jp>
2022-11-01 00:00:19 +0900

    20221031

---
+++ de12/python/game.py
@@ -0,0 +1,29 @@
+from random import random
+
+
+print("ブラックジャックへようこそ")
+name=input("名前を入力してください")
+
+print("ゲームの説明を聞く"）
+
+import random
+card = random.randint(1,13)
+print("あなたの引いたカードは,card")
+
+card=21
+if card >=22:
+  print("ゲームオーバー")
+
+elif:
+  print("ヒット")
+import random
+card = random.randint(1,13)
+print("あなたの引いたカードは,card")
+
+else:
+  print("スタンド")
+
+  print("次のプレイヤーに変わってください")
+
+
+
