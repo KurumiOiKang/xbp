794e61718822c107809b4a38520c22dad4bf7d22
KurumiOiKang <r202202074om@jindai.jp>
2022-11-03 20:57:13 +0900

    20221103

--- de12/python/game.py
+++ de12/python/game.py
@@ -1,29 +1,199 @@
-from random import random
+import random
+import time
+
+mark = ["spead","heart","diamond","club"]
 
 
 print("ブラックジャックへようこそ")
-name=input("名前を入力してください")
+
 
 print("ゲームの説明を聞く"）
-
-import random
-card = random.randint(1,13)
-print("あなたの引いたカードは,card")
-
-card=21
-if card >=22:
-  print("ゲームオーバー")
-
-elif:
-  print("ヒット")
-import random
-card = random.randint(1,13)
-print("あなたの引いたカードは,card")
+if players=YES
+print("【遊び方】")
+print("21点を超えずに合計点が21に近い人が勝ちです。同じ点数の場合は引き分けです。")
+print("ヒット＝カードを追加する")
+print("スタンド＝カードを追加しない")
+print("自分のターンが終わったら次の人に変わりましょう")
 
 else:
-  print("スタンド")
+   print("さあ、ゲーム会場に移りましょう")　
+   
+class Deck:
+    def__init__(self):
+       self.cards = [
+           [i,j]
+           for i in range(1,14)
+           for j in mark
+           ]
+      random.shuffle(self.cards)
+    def emission(self):
+        emission_card = self.cards.pop()
+        return emission_card
 
-  print("次のプレイヤーに変わってください")
+ # deck = Deck()
+ # print(len(desk.cards))
+ # print(deck.cards)     
+
+ def str_point(card):
+     if card[0] > 10:
+        return 10
+     elif card[0] == 1:
+        return 11
+     else:
+        return card[0]
+
+def count_card(cards):
+    point = [str_point(i) for i in cards]
+    cnt = point.count(11)
+    card=sum = sum(point)
+    for i in range(cnt):
+        if card_sum > 21:
+           card_sum -= 10
+    return card_sum   
+
+
+class player():
+    def __init__(self):
+        global deck
+
+        self.cards = []
+        card1 = deck.emission()
+        self.cards.append(card1)
+        card2 = desk.emission()
+        self.cards.append(card2)
+
+    def card_open(self):
+        print("PL:self.cards")
+
+    def point_open(self):
+        print("PL:",count_card(self.cards))
+
+    def draw(self):
+        card = deck.emission()
+        self.cards.append(card)
+
+class Deerer(Player):
+    def card_open(self):
+            print("DR:",self.cards)
+
+    def point_open(self):
+        print("DR:",count_card(self.cards))
+
+    def card_close(self):
+        print("DL:",self.cards[0],"[*,*]")
+
+    def point_close(self):
+        print("DL:",str_point(self.cards[0]))
+
+def game(desk):
+
+   player = Player()
+   player.card_open()
+   player.point_open()
+
+   deerer = Deerer()
+   deerer.card_close()
+   deerer.point_close()
+
+   select = 0
+  
+   while count_card(player.cards) <= 21:
+       time.sleep(1)
+
+       if count_card(player.cards) == 21:
+           print("BJ")
+         　 return 1
+
+       select = input("ヒットするYES:1...")
+
+       if select == 1:
+          player.draw()
+          player.card_open()
+          player.point_open() 
+       else:
+           break
+   else:
+       print("BERST")
+       return 0
+
+   time.sleep(1)
+   deerer.card_open()
+   time.sleep(1)
+   deerer.point_open()
+
+   while count_card(deerer.cards) < 16:
+       time.sleep(1)
+       deerer.draw()
+
+       time.sleep(1)
+       deerer.card_open()
+
+       time.sleep(1)
+       deerer.point_open()
+
+       if count_card(deerer.cards) > 21:
+           print("BJ")
+           return 1
+   time.sleep(1)
+
+   if count_card(deerer.cards) < count_card(player.cards)
+       return 1 
+   elif count_card(player.cards) < count_card(deerer.cards):
+       return 0
+   else:
+       return -1
+                
+start = 1
+win_lose=[0,0]
+
+deck = Deck()
+
+while len(deck.cards) > 10:
+    start = input("ゲームを始めましょう！YES1...")
+    if start != 1:
+       print("終了しました")
+       break
+    print("ゲームを始めます")
+
+    rslt = game(deck)
+    if rslt== 1:
+      win_lose[0] += 1
+      print("winner")
+      print(win_lose[0],"勝",win_lose[1],"敗")
+    elif rslt ==0: 
+         win_lose[1] +=1
+         print("YOU　LOSE")
+         print(win_lose[0]"勝",win_lose[1],"敗")
+    else:
+       print("DRAW")
+       print(win_lose[0],"勝",win_lose[1],"敗")
+else:
+     print("カードが無くなりました")
+
+   
+
+
+
+          
+            
+                    
+                  
+
+          
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
 
 
 
---
+++ de12/python/sousecode.py
@@ -0,0 +1,101 @@
+import calendar
+import datetime
+
+import PySimpleGUI as sg
+
+
+sg.theme('LightBlue6')
+today = datetime.date.today()
+
+def create_layout(cal_date):
+    weekday = ['SUNDAY','MONDAY', 'TUESDAY', 'WEDNDSDAY', 'THURSDAY', 'FRIDAY', 'SATURDAY']  
+    cal = calendar.Calendar(firstweekday=6)
+    days = cal.monthdatescalendar(cal_date.year, cal_date.month)
+    layout = [[sg.Text(cal_date.year, font=(None, 13, 'bold'))],
+              [sg.Push(), sg.Button('<<'), sg.Button('<'), sg.Text(cal_date.month, font=(None, 30)), sg.Button('>'), sg.Button('>>'), sg.Push()]]
+    inner = []
+    
+    for week in weekday:
+        inner.append(sg.Text(week, size=(11,1), text_color='yellow', background_color='lightpink', justification='center'))
+    layout.append(inner.copy())
+
+    def date_judgement(i, day):
+        if day == today:
+            return sg.Text(day.day, size=(11,4), justification='right', text_color='white', background_color='gray')
+        elif i == 0 and day.month == cal_date.month:
+            return sg.Text(day.day, size=(11,4), justification='right', text_color='red')
+        elif i == 6 and day.month == cal_date.month:
+            return sg.Text(day.day, size=(11,4), justification='right', text_color='blue')
+        elif day.month == cal_date.month:
+            return sg.Text(day.day, size=(11,4), justification='right')
+        elif i == 0:
+            return sg.Text(day.day, size=(11,4), justification='right', text_color='#ff9999')
+        elif i == 6:
+            return sg.Text(day.day, size=(11,4), justification='right', text_color='#9999ff')
+        else:
+            return sg.Text(day.day, size=(11,4), justification='right', text_color='#cccccc')
+
+    for row in days:
+        inner = []
+        for i, day in enumerate(row):
+            sg_text = date_judgement(i, day)
+            inner.append(sg_text)
+        layout.append(inner.copy())
+    return layout
+
+def main():
+    cal_date = today
+    layout = create_layout(cal_date)
+    window = sg.Window('Simple Calendar', layout)
+
+    while True:
+        event, _ = window.read()
+        if event == sg.WIN_CLOSED:
+            break
+        elif event == '<':
+            cal_date = datetime.date(cal_date.year, cal_date.month, 1) - datetime.timedelta(days=1)
+        elif event == '>':
+            cal_date = datetime.date(cal_date.year, cal_date.month, 28) + datetime.timedelta(days=4)
+        elif event == '<<':
+            cal_date = datetime.date(cal_date.year - 1, cal_date.month, 1)
+        elif event == '>>':
+            cal_date = datetime.date(cal_date.year + 1, cal_date.month, 1)
+        window.close()
+        window = sg.Window('Simple Calendar', create_layout(cal_date))
+    window.close()
+
+if __name__ == '__main__':
+    main()
+
+import datetime
+import heapq as hq
+
+
+# タスクの情報を保持するクラス
+class Task:
+    # 本来は引数要素の型チェックを行うが省略
+    def __init__(self, name, datetime):
+        self.name = name
+        self.datetime = datetime
+    
+    def __lt__(self, other):
+        if not isinstance(other, Task):
+            return NotImplemented
+        return self.datetime < other.datetime
+
+
+tasks = []
+
+# タスクの追加
+hq.heappush(tasks, Task("タスクA", datetime.datetime(2021, 4, 1)))
+hq.heappush(tasks, Task("タスクA", datetime.datetime(2021, 6, 1)))
+hq.heappush(tasks, Task("タスクA", datetime.datetime(2021, 5, 1)))
+
+# タスクが存在する間ループ
+while tasks:
+    # タスクを取り出したくない場合はtask[0]を参照すればよい
+    # 実行時刻を超えたらタスクをキューから取り出して処理する
+    if tasks[0] <= datetime.datetime.now():
+        task = hq.heappop(tasks)
+        # タスクに対する何らかの処理
+    # スリープ処理等
